4edbde8b785b06b16a70149217213d6a6e7dd66c
fpp-7 <felipee.7@live.com>
2023-06-28 18:51:54 -0300

    ManyToMany

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -23,8 +25,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
-	private Set<Category> products = new HashSet<>();
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category () {
 		
@@ -77,7 +80,7 @@ public class Category implements Serializable {
 		return true;
 	}
 
-	public Set<Category> getProducts() {
+	public Set<Product> getProducts() {
 		return products;
 	}
 	
